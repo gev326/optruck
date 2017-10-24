49e15e4f03feafbe6916b440914f09549928b8c6
Rob Gonnella <robgonnella@gmail.com>
2017-10-23 23:41:49 -0700

    Update config for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-require 'faker'
-
 users = [
   {
     :first_name => 'Admin',
@@ -24,36 +22,36 @@ users.each do |u|
   User.create(u)
 end
 
-current_user = User.first
+# current_user = User.first
 
-drivers = []
+# drivers = []
 
-10.times do
-  covered = [true, false].sample
-  id = covered ? current_user.id : nil
-  driver = {
-    :first_name => Faker::Name.first_name,
-    :last_name => Faker::Name.last_name,
-    :current_state => Faker::Address.state,
-    :desired_state => Faker::Address.state,
-    :driver_id_tag => Faker::Number.number(6),
-    :driver_phone => Faker::Number.number(10),
-    :driver_truck_type => ['r53', 'flat'].sample,
-    :driver_contract_number => Faker::Number.number(5),
-    :active => [true, false].sample,
-    :driver_company => Faker::Company.name,
-    :backhaul => [true, false].sample,
-    :Covered => covered,
-    :user_id => id,
-    :reeferunit => [true, false].sample,
-    :insurance =>Faker::Company.name
-  }
-  drivers.push(driver)
-end
+# 10.times do
+#   covered = [true, false].sample
+#   id = covered ? current_user.id : nil
+#   driver = {
+#     :first_name => Faker::Name.first_name,
+#     :last_name => Faker::Name.last_name,
+#     :current_state => Faker::Address.state,
+#     :desired_state => Faker::Address.state,
+#     :driver_id_tag => Faker::Number.number(6),
+#     :driver_phone => Faker::Number.number(10),
+#     :driver_truck_type => ['r53', 'flat'].sample,
+#     :driver_contract_number => Faker::Number.number(5),
+#     :active => [true, false].sample,
+#     :driver_company => Faker::Company.name,
+#     :backhaul => [true, false].sample,
+#     :Covered => covered,
+#     :user_id => id,
+#     :reeferunit => [true, false].sample,
+#     :insurance =>Faker::Company.name
+#   }
+#   drivers.push(driver)
+# end
 
-Driver.delete_all
-drivers.each do |d|
-  Driver.create(d)
-end
+# Driver.delete_all
+# drivers.each do |d|
+#   Driver.create(d)
+# end
 
-puts "Successfully added #{drivers.length} drivers to database"
+# puts "Successfully added #{drivers.length} drivers to database"
